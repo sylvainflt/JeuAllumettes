46c21d150f1c89b62744ca6fd7623c60cc6eaced
sylvain foucault <sylvain@acer.lan>
2023-10-06 09:04:05 +0200

    avec interface graphique

--- src/allumettes/Jeu.java
+++ src/allumettes/Jeu.java
@@ -1,11 +1,65 @@
 package allumettes;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Scanner;
 
-public class Jeu {
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Jeu extends JPanel implements ActionListener{
 
 	int nbAllumettes= 17;
 	
+	protected JButton b1, b2, b3;
+	protected JPanel allumettes;
+	protected JPanel buttons;
+	protected JLabel allumettesLabel;
+	
+	public Jeu() {
+		b1 = new JButton("1");
+        b1.setSize(20, 20);
+        b1.setActionCommand("1");
+        b1.addActionListener(this);
+        
+        b2 = new JButton("2");
+        b2.setSize(20, 20);
+        b2.setActionCommand("2");
+        b2.addActionListener(this);
+        
+        b3 = new JButton("3");
+        b3.setSize(20, 20);
+        b3.setActionCommand("3");
+        b3.addActionListener(this);
+        
+        allumettes = new JPanel();
+        allumettesLabel = new JLabel(afficherAllumettesString());
+        allumettes.add(allumettesLabel);
+
+        buttons = new JPanel();
+        buttons.add(b1, BorderLayout.LINE_START);
+        buttons.add(b2, BorderLayout.CENTER);
+        buttons.add(b3, BorderLayout.LINE_END);
+	}	
+	
+	public JPanel getAllumettes() {
+		return allumettes;
+	}
+
+	public void setAllumettes(JPanel allumettes) {
+		this.allumettes = allumettes;
+	}
+
+	public JPanel getButtons() {
+		return buttons;
+	}
+
+	public void setButtons(JPanel buttons) {
+		this.buttons = buttons;
+	}
+
 	public void afficherAllumettes() {
 	
 		for(int i=0; i<nbAllumettes; i++) {
@@ -15,6 +69,15 @@ public class Jeu {
 		
 	}
 	 
+	public String afficherAllumettesString() {
+		String s = "";
+		for(int i=0; i<nbAllumettes; i++) {
+			s += "|";
+		}
+		return s;
+		
+	}
+	
 	public void commencerJeu() {
 	
 		System.out.println("Veuillez entrer le nombre d'allumettes : ");
@@ -59,5 +122,27 @@ public class Jeu {
 		
 	}
 	
+	public void actionPerformed(ActionEvent e) {
+        if ("1".equals(e.getActionCommand())) {
+        	nbAllumettes -= 1;
+        	allumettesLabel.setText(afficherAllumettesString());
+        } else if ("2".equals(e.getActionCommand())) {
+        	nbAllumettes -= 2;
+        	allumettesLabel.setText(afficherAllumettesString());
+        } else if ("3".equals(e.getActionCommand())) {
+        	nbAllumettes -= 3;
+        	allumettesLabel.setText(afficherAllumettesString());
+        }
+        if(nbAllumettes <= 0) {
+        	allumettesLabel.setText("le joueur a gagné");				
+		}else {
+			nbAllumettes -= Ordinateur.propositionOrdinateur();
+			allumettesLabel.setText(afficherAllumettesString());
+			if(nbAllumettes <= 0) {
+	        	allumettesLabel.setText("l'ordinateur a gagné");				
+			}
+		}
+        
+    }
 	
 }
--- src/allumettes/TestJeu.java
+++ src/allumettes/TestJeu.java
@@ -1,11 +1,36 @@
 package allumettes;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 public class TestJeu {
 
+	private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("Jeu des allumettes");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        
+        
+        Jeu j = new Jeu();
+        frame.getContentPane().add(new JLabel("veuillez choisir un nombre d'allumettes à retirer"), BorderLayout.PAGE_START);
+        frame.getContentPane().add(j.getAllumettes(), BorderLayout.CENTER);
+        frame.getContentPane().add(j.getButtons(), BorderLayout.PAGE_END);
+        
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+        //frame.setSize(350, 150);
+    }
+	
 	public static void main(String[] args) {
-		Jeu a= new Jeu();
-		a.menuJeu();
+		
 
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
 	}
 
 }
